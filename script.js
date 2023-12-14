b9a90ee894dad796691bc54e7c8ac9ae95ee3c7b
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2023-12-14 13:34:22 +0400

    Initialized query selectors
    Changed clearers' class

---
+++ script.js
@@ -0,0 +1,7 @@
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const deleter = document.getElementById('delete')
+const clearer = document.getElementById('clear')
+const equals = document.getElementsByClassName('equals')
+const pastInput = document.getElementById('past-input')
+const currentInput = document.getElementById('current-input')
