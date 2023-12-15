6bda71ac18b99316990532f4da665ded1f3e8ee9
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2023-12-15 21:02:56 +0400

    Enable entering digits

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const numbers = document.querySelectorAll('.number')
-const operators = document.querySelectorAll('.operator')
+let input1 = ''
+let input2 = ''
+let operator = ''
+
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
 const deleter = document.getElementById('delete')
 const clearer = document.getElementById('clear')
 const equals = document.getElementsByClassName('equals')
-const pastInput = document.getElementById('past-input')
-const currentInput = document.getElementById('current-input')
+const topInput = document.getElementById('past-input')
+const bottomInput = document.getElementById('current-input')
+
+function addNumber(number) {
+    if (bottomInput.textContent === '0') {
+        bottomInput.textContent = null
+    }
+    bottomInput.textContent += number
+}
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => addNumber(button.textContent))
+)
+
+
