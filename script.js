93b4e2de4bc6110a64ef356ae91cf14f66bef344
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2023-12-16 01:44:51 +0400

    Add operate function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator')
 const deleter = document.getElementById('delete')
 const clearer = document.getElementById('clear')
-const equals = document.getElementsByClassName('equals')
+const equals = document.getElementById('equals')
 const topInput = document.getElementById('past-input')
 const bottomInput = document.getElementById('current-input')
 
@@ -21,14 +21,34 @@ function setOperator(operation) {
     input1 = bottomInput.textContent
     operator = operation
     topInput.textContent = `${operator} ${input1}`
+    bottomInput.textContent = '0'
 }
 
 function evaluate() {
-    
+    input2 = bottomInput.textContent
+    topInput.textContent = `= ${input2} ${operator} ${input1}`
+    bottomInput.textContent = operate()
 }
 
 function resetInput() {
     bottomInput.textContent = null
+}
+
+function operate() {
+    switch (operator) {
+        case "+":
+            return (+input1 + +input2).toString()
+            break;
+        case "-":
+            return (+input1 - +input2).toString()
+            break;
+        case "×":
+            return (+input1 * +input2).toString()
+            break;
+        case "÷":
+            return (+input1 / +input2).toString()
+            break;
+    }
 }
 
 numberButtons.forEach((button) =>
@@ -39,4 +59,4 @@ operatorButtons.forEach((button) =>
     button.addEventListener('click', () => setOperator(button.textContent))
 )
 
-equals.addEventListener('click', () => evaluate())
+equals.addEventListener('click', evaluate)
